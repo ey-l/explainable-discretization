501cf4863b15097e80154597f4b430ac78154524
eugenieshine <yujinglai0707@gmail.com>
2024-02-14 09:54:05 -0500

    implement sse

--- code/bucket.py
+++ code/bucket.py
@@ -2,15 +2,20 @@ import numpy as np
 import sys
 import os
 from typing import List, Union, Any, Tuple, Dict
+from collections import OrderedDict as odict
 
 # Project path
 ppath = sys.path[0] + '/../'
 
 class Bucket:
     """
-    Class for discret
+    Class for bucket.
     """
     def __init__(self, startpoint:float, endpoint:float, count:int, label:Union[str, int]):
+        """
+        Initialize the bucket with startpoint, endpoint, count, and label.
+        Value included in the bucket is in the range [startpoint, endpoint].
+        """
         self.startpoint = startpoint
         self.endpoint = endpoint
         self.count = count
@@ -28,6 +33,15 @@ class Bucket:
     def __le__(self, __value: object) -> bool:
         return self.endpoint <= __value.startpoint
     
+    def __gt__(self, __value: object) -> bool:
+        return self.startpoint > __value.endpoint
+    
+    def __ge__(self, __value: object) -> bool:
+        return self.startpoint >= __value.endpoint
+    
+    def value_in_bucket(self, value:float) -> bool:
+        return self.startpoint <= value and value <= self.endpoint
+    
 
 class BucketList:
     """
@@ -35,6 +49,60 @@ class BucketList:
     """
     def __init__(self, buckets:List[Bucket]):
         self.buckets = buckets
+
+    def get_bucket_containing_q(self, q:float) -> Bucket:
+        """
+        Get the bucket that contains the value q.
+        """
+        for bucket in self.buckets:
+            if bucket.value_in_bucket(q): return bucket
+        return None
     
-    def cal_sse(self, other:List[Bucket]) -> float:
-        pass
+    def cal_sse(self, values:odict) -> float:
+        """
+        Calculate the sum of squared errors (SSE) for the given values.
+        :param values: An ordered dictionary of values and their counts.
+        """
+        sse = 0
+        dict_keys = list(values.keys())
+        start_value = dict_keys[0]
+        end_value = dict_keys[-1]
+        for q in range(start_value, end_value+1):
+            # Get fp_q
+            fp_q = 0
+            if q in dict_keys: fp_q = values[q]
+            # Get the bucket that contains q
+            bucket = self.get_bucket_containing_q(q)
+            # If the bucket is None, continue
+            if not bucket: continue
+            eb_q = bucket.count
+            # Calculate the squared error
+            sse += (fp_q - (eb_q / (bucket.endpoint - bucket.startpoint + 1)))**2
+        return sse
+
+
+if __name__ == '__main__':
+    # Test Bucket class
+    b1 = Bucket(0, 0, 4, 'A')
+    b2 = Bucket(10, 50, 9, 'B')
+    b3 = Bucket(5, 7, 30, 'C')
+    print(b1)
+    print(b2)
+    print(b3)
+    print(b1 == b2)
+    print(b1 < b2)
+    print(b1 < b3)
+    print(b1 > b2)
+    print(b1 > b3)
+    print(b1.value_in_bucket(2))
+    print(b1.value_in_bucket(3))
+    print(b1.value_in_bucket(4))
+    print(b1.value_in_bucket(5))
+
+    d = {0:4,10:2,20:2,30:2,40:2,50:1}
+    print(sorted(d.items()))
+    od = odict(sorted(d.items()))
+    print(list(od.keys())[0])
+
+    bls = BucketList([b1, b2])
+    print(bls.cal_sse(od))
