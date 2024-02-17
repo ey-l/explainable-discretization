bac3eb709f03b737b30f74e72c0209399afa302f
eugenieshine <yujinglai0707@gmail.com>
2024-02-17 15:39:03 -0500

    add kl-div code and tests

--- code/bucket.py
+++ code/bucket.py
@@ -48,27 +48,35 @@ class BucketList:
     """
     Class for a list of buckets
     """
-    def __init__(self, bins, values:List=None, buckets:List[Bucket]=None):
+    def __init__(self, bins, values:List, buckets:List[Bucket]=None):
         # Bins to apply discretization to data
         self.bins = bins
         # Buckets to calculate errors
         if buckets is not None: self.buckets = buckets
-        elif values is not None: self.buckets = self._create_buckets(bins, values)
+        elif values is not None: 
+            self.buckets = self._create_buckets(bins, values)
+            self.value_odict = self._create_value_odict(values)
         else: raise ValueError('Either buckets or values must be provided.')
+        
+        self.start_value = sorted(values)[0]
+        self.end_value = sorted(values)[-1]
 
     def _create_buckets(self, bins, values:List) -> List[Bucket]:
         """
         Create buckets from the bins and unique values.
         """
         buckets = []
-        unique_values = np.array(list(set(values)))
+        unique_values = np.array(sorted(list(set(values))))
         counter = Counter(values)
         for i in range(len(bins)-1):
-            if len(buckets) == 0: startpoint = bins[i]
+            if len(buckets) == 0: startpoint = min(unique_values)
             # Use the next unique value as the startpoint
             else: 
                 idx = np.searchsorted(unique_values,[bins[i],],side='right')[0]
+                #print("index",idx)
                 startpoint = unique_values[idx]
+                #print("unique_values",unique_values)
+                #print("startpoint",startpoint)
             endpoint = bins[i+1]
             count = 0
             for value in unique_values:
@@ -76,6 +84,15 @@ class BucketList:
             buckets.append(Bucket(startpoint, endpoint, count, None))
         return buckets
 
+    def _create_value_odict(self, values:List) -> odict:
+        """
+        Create an ordered dictionary of values and their counts.
+        """
+        value_dict = {}
+        for bucket in self.buckets:
+            value_dict[bucket.startpoint] = bucket.count
+        return odict(sorted(value_dict.items()))
+    
     def get_bucket_containing_q(self, q:float) -> Bucket:
         """
         Get the bucket that contains the value q.
@@ -91,9 +108,7 @@ class BucketList:
         """
         sse = 0
         dict_keys = list(values.keys())
-        start_value = dict_keys[0]
-        end_value = dict_keys[-1]
-        for q in range(start_value, end_value+1):
+        for q in range(self.start_value, self.end_value+1):
             # Get fp_q
             fp_q = 0
             if q in dict_keys: fp_q = values[q]
@@ -105,6 +120,24 @@ class BucketList:
             # Calculate the squared error
             sse += (fp_q - (eb_q / (bucket.endpoint - bucket.startpoint + 1)))**2
         return sse
+    
+    def cal_KLDiv(self, ref_bucket_list) -> float:
+        """
+        Calculate the Kullback-Leibler divergence (KL-divergence) for the given reference bucket list.
+        :param ref_bucket_list: The reference bucket list.
+        """
+        kl_div = 0
+        # Get the shorter bucket list
+        shorter_length = 0
+        if len(self.buckets) < len(ref_bucket_list.buckets): shorter_length = len(self.buckets)
+        else: shorter_length = len(ref_bucket_list.buckets)
+        # Calculate the KL-divergence
+        for i in range(shorter_length):
+            p = self.buckets[i].count
+            q = ref_bucket_list.buckets[i].count
+            if p == 0 or q == 0: continue
+            kl_div += p * np.log(p / q)
+        return kl_div
 
 
 if __name__ == '__main__':
@@ -126,15 +159,31 @@ if __name__ == '__main__':
     print(b1.value_in_bucket(5))
 
     d = {0:4,10:2,20:2,30:2,40:2,50:1}
+    values = [0,20,30,20,30,40,10,40,50,0,0,0]
     print(sorted(d.items()))
     od = odict(sorted(d.items()))
     print(list(od.keys())[0])
 
-    bls = BucketList(bins=[0, 10, 50], buckets=[b1, b2])
+    bls = BucketList(bins=[0, 10, 50], values=values, buckets=[b1, b2])
     print(bls.cal_sse(od))
 
-    bls = BucketList(bins=[0,50], buckets=[b3])
+    bls = BucketList(bins=[0,50], values=values, buckets=[b3])
     print(bls.cal_sse(od))
 
-    bls = BucketList(bins=[-1, 140, 200], values=[0, 0, 0, 140, 141, 200])
-    print(bls.buckets)
+    values = [0, 0, 0, 102, 102, 102, 102, 102, 140, 141, 151, 200]
+    glucose_gpt = BucketList(bins=[-1, 140, 200], values=values)
+    print(glucose_gpt.buckets)
+    print(glucose_gpt.value_odict)
+
+    glucose0 = BucketList(bins=[-1, 100, 200], values=values)
+    print(glucose0.buckets)
+    print(glucose0.cal_sse(glucose_gpt.value_odict))
+    print("KL-divergence:",glucose0.cal_KLDiv(glucose_gpt))
+
+    glucose1 = BucketList(bins=[-1, 150, 200], values=values)
+    print(glucose1.buckets)
+    print(glucose1.cal_sse(glucose_gpt.value_odict))
+    print("KL-divergence:",glucose1.cal_KLDiv(glucose_gpt))
+
+    bls = BucketList(bins=[0, 10, 50], values=values, buckets=[b1, b2])
+    print(bls.cal_KLDiv(bls))
