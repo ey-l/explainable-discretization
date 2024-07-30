039f2f5407b8a7b0e763224d9f63337bde9d5445
eugenieshine <yujinglai0707@gmail.com>
2024-07-30 16:40:41 -0400

    add 1d cluster

---
+++ code/1d_cluster.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import sys
+import os
+import matplotlib.pyplot as plt
+import graphviz
+import itertools
+#from sklearn.inspection import DecisionBoundaryDisplay
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.model_selection import train_test_split
+from sklearn.impute import KNNImputer
+# import Kmeans from sklearn
+from sklearn.cluster import KMeans
+from sklearn import tree
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, mean_absolute_error
+from typing import List, Union, Any, Tuple, Dict
+import time
+from permetrics.regression import RegressionMetric
+import oapackage
+from scipy.stats import wasserstein_distance, binned_statistic
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Project path
+ppath = sys.path[0] + '/../'
+sys.path.append(os.path.join(ppath, 'code'))
+
+df = pd.read_csv(os.path.join(ppath, 'data', 'uciml_pima-indians-diabetes-database', 'diabetes.csv'))
